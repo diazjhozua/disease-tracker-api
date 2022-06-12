9413eaf4c45a8590634293d809455e95be080a9b
Jhozua Diaz <diaz.jhozua@gmail.com>
2022-06-12 19:29:19 +0800

    Add Organization Model

---
+++ Models/Organization.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace disease_tracker_api.Models
+{
+    public class Organization
+    {
+        public Organization()
+        {
+            DateCreated = DateTime.Now;
+        }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string MobileNo { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Region { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Country { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Province { get; set; }
+
+        [Required]
+        public int ZipCode { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Address { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime? DateModified { get; set; }
+
+        [StringLength(1000)]
+        #nullable enable
+        public string? ArchiveReason { get; set; }
+
+        public bool IsArchived { get; set; } = false;
+
+    }
+}
