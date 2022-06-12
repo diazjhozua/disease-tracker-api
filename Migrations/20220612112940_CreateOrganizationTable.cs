8a272d22b5b6bd2189a9a5a86b028eefa85c1bc5
Jhozua Diaz <diaz.jhozua@gmail.com>
2022-06-12 19:30:17 +0800

    Created Organization Migration

---
+++ Migrations/20220612112940_CreateOrganizationTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace disease_tracker_api.Migrations
+{
+    public partial class CreateOrganizationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
