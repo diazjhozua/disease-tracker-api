085f3154619080cb52006fdeeb4c8ad0f9923a2d
Jhozua Diaz <diaz.jhozua@gmail.com>
2022-07-16 19:03:45 +0800

    Edit IAuth

--- Data/IAuthRepository.cs
+++ Data/IAuthRepository.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using disease_tracker_api.Models;
 
 namespace disease_tracker_api.Data
 {
     public interface IAuthRepository
     {
-        
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
     }
 }
