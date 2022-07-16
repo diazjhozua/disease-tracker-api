aeaec63357c869779c64d4000872b86e35474e65
Jhozua Diaz <diaz.jhozua@gmail.com>
2022-07-16 19:23:49 +0800

    Added migration for updating column name field of email to users table

---
+++ Migrations/20220716111853_Update-ColumnName-Of-Email-Field-To-User-Table.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace disease_tracker_api.Migrations
+{
+    public partial class UpdateColumnNameOfEmailFieldToUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Users",
+                newName: "Email");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "email");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -8,7 +8,7 @@ namespace disease_tracker_api.Models
     public class User
     {
         public int Id { get; set; }
-        public string email { get; set; }
+        public string Email { get; set; }
         public int MyProperty { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
